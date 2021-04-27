4193aed7f351c792fbe93a25752f2fdbbedf5c46
Medpej Calibração - 01 <calibracao.01@medpej.local>
2021-04-27 09:19:58 -0300

    Adicionar arquivos de projeto.

---
+++ FrontEnd/frmCadProduto.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace FrontEnd
+{
+    partial class frmCadProduto
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnExcluir = new System.Windows.Forms.Button();
+            this.BtnAtualizar = new System.Windows.Forms.Button();
+            this.BtnLimpar = new System.Windows.Forms.Button();
+            this.dgvProduto = new System.Windows.Forms.DataGridView();
+            this.nudQtdEstoque = new System.Windows.Forms.NumericUpDown();
+            this.Quantidade = new System.Windows.Forms.Label();
+            this.nudPreco = new System.Windows.Forms.NumericUpDown();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtIDProduto = new System.Windows.Forms.TextBox();
+            this.Nome = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.BtnInserir = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProduto)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudQtdEstoque)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudPreco)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // BtnExcluir
+            // 
+            this.BtnExcluir.Enabled = false;
+            this.BtnExcluir.Location = new System.Drawing.Point(653, 118);
+            this.BtnExcluir.Name = "BtnExcluir";
+            this.BtnExcluir.Size = new System.Drawing.Size(111, 32);
+            this.BtnExcluir.TabIndex = 27;
+            this.BtnExcluir.Text = "Excluir";
+            this.BtnExcluir.UseVisualStyleBackColor = true;
+            this.BtnExcluir.Click += new System.EventHandler(this.BtnExcluir_Click);
+            // 
+            // BtnAtualizar
+            // 
+            this.BtnAtualizar.Enabled = false;
+            this.BtnAtualizar.Location = new System.Drawing.Point(377, 118);
+            this.BtnAtualizar.Name = "BtnAtualizar";
+            this.BtnAtualizar.Size = new System.Drawing.Size(111, 32);
+            this.BtnAtualizar.TabIndex = 26;
+            this.BtnAtualizar.Text = "Atualizar";
+            this.BtnAtualizar.UseVisualStyleBackColor = true;
+            this.BtnAtualizar.Click += new System.EventHandler(this.BtnAtualizar_Click);
+            // 
+            // BtnLimpar
+            // 
+            this.BtnLimpar.Location = new System.Drawing.Point(196, 118);
+            this.BtnLimpar.Name = "BtnLimpar";
+            this.BtnLimpar.Size = new System.Drawing.Size(111, 32);
+            this.BtnLimpar.TabIndex = 25;
+            this.BtnLimpar.Text = "Limpar";
+            this.BtnLimpar.UseVisualStyleBackColor = true;
+            this.BtnLimpar.Click += new System.EventHandler(this.BtnLimpar_Click);
+            // 
+            // dgvProduto
+            // 
+            this.dgvProduto.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvProduto.Location = new System.Drawing.Point(13, 183);
+            this.dgvProduto.Name = "dgvProduto";
+            this.dgvProduto.Size = new System.Drawing.Size(751, 294);
+            this.dgvProduto.TabIndex = 24;
+            this.dgvProduto.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvProduto_CellClick);
+            // 
+            // nudQtdEstoque
+            // 
+            this.nudQtdEstoque.Location = new System.Drawing.Point(605, 70);
+            this.nudQtdEstoque.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.nudQtdEstoque.Name = "nudQtdEstoque";
+            this.nudQtdEstoque.Size = new System.Drawing.Size(68, 20);
+            this.nudQtdEstoque.TabIndex = 18;
+            // 
+            // Quantidade
+            // 
+            this.Quantidade.AutoSize = true;
+            this.Quantidade.Location = new System.Drawing.Point(525, 73);
+            this.Quantidade.Name = "Quantidade";
+            this.Quantidade.Size = new System.Drawing.Size(68, 13);
+            this.Quantidade.TabIndex = 23;
+            this.Quantidade.Text = "Quantidade :";
+            // 
+            // nudPreco
+            // 
+            this.nudPreco.DecimalPlaces = 2;
+            this.nudPreco.Location = new System.Drawing.Point(117, 71);
+            this.nudPreco.Maximum = new decimal(new int[] {
+            100000,
+            0,
+            0,
+            0});
+            this.nudPreco.Name = "nudPreco";
+            this.nudPreco.Size = new System.Drawing.Size(120, 20);
+            this.nudPreco.TabIndex = 16;
+            this.nudPreco.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(117, 39);
+            this.txtNome.MaxLength = 100;
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(556, 20);
+            this.txtNome.TabIndex = 14;
+            // 
+            // txtIDProduto
+            // 
+            this.txtIDProduto.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.txtIDProduto.Location = new System.Drawing.Point(117, 9);
+            this.txtIDProduto.Name = "txtIDProduto";
+            this.txtIDProduto.Size = new System.Drawing.Size(107, 20);
+            this.txtIDProduto.TabIndex = 22;
+            // 
+            // Nome
+            // 
+            this.Nome.AutoSize = true;
+            this.Nome.Location = new System.Drawing.Point(50, 45);
+            this.Nome.Name = "Nome";
+            this.Nome.Size = new System.Drawing.Size(35, 13);
+            this.Nome.TabIndex = 21;
+            this.Nome.Text = "Nome";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(51, 77);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 19;
+            this.label3.Text = "Preço";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(1, -8);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 23);
+            this.label2.TabIndex = 20;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "ID Produto";
+            // 
+            // BtnInserir
+            // 
+            this.BtnInserir.Location = new System.Drawing.Point(13, 118);
+            this.BtnInserir.Name = "BtnInserir";
+            this.BtnInserir.Size = new System.Drawing.Size(111, 32);
+            this.BtnInserir.TabIndex = 15;
+            this.BtnInserir.Text = "Inserir";
+            this.BtnInserir.UseVisualStyleBackColor = true;
+            this.BtnInserir.Click += new System.EventHandler(this.BtnInserir_Click);
+            // 
+            // frmCadProduto
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(894, 489);
+            this.Controls.Add(this.BtnExcluir);
+            this.Controls.Add(this.BtnAtualizar);
+            this.Controls.Add(this.BtnLimpar);
+            this.Controls.Add(this.dgvProduto);
+            this.Controls.Add(this.nudQtdEstoque);
+            this.Controls.Add(this.Quantidade);
+            this.Controls.Add(this.nudPreco);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.txtIDProduto);
+            this.Controls.Add(this.Nome);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BtnInserir);
+            this.Name = "frmCadProduto";
+            this.Text = "Cadastro de Produto";
+            this.Load += new System.EventHandler(this.frmCadProduto_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProduto)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudQtdEstoque)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudPreco)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnExcluir;
+        private System.Windows.Forms.Button BtnAtualizar;
+        private System.Windows.Forms.Button BtnLimpar;
+        private System.Windows.Forms.DataGridView dgvProduto;
+        private System.Windows.Forms.NumericUpDown nudQtdEstoque;
+        private System.Windows.Forms.Label Quantidade;
+        private System.Windows.Forms.NumericUpDown nudPreco;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtIDProduto;
+        private System.Windows.Forms.Label Nome;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button BtnInserir;
+    }
+}
+
